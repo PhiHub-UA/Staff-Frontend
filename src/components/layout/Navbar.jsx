be72110a80a844d7577afde2ae6cd70d7b0da90e
d479 <daniel.madureira@ua.pt>
2024-05-15 18:47:04 +0100

    fixed navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -16,12 +16,13 @@ import { logo } from "../../assets";
 import { CgLogOut } from "react-icons/cg";
 import axios from "../../api/axios";
 
-import {useQuery} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useStaffStore } from "../../stores/staffStore";
 
-function Navbar( ) {
-
+function Navbar() {
   const login = useStaffStore((state) => state.login) || false;
+  const user = useStaffStore((state) => state.username);
+  const logout = useStaffStore((state) => state.logout);
 
   const _ = useQuery({
     queryKey: ["user"],
@@ -44,8 +45,6 @@ function Navbar( ) {
     },
   });
 
-  const user = useStaffStore((state) => state.username);
-  const logout = useStaffStore((state) => state.logout);
 
   return (
     <nav className="p-4 bg-transparent">
@@ -67,7 +66,7 @@ function Navbar( ) {
                 Register new Staff
               </Link>
             </NavbarItem>
-            <NavbarItem >
+            <NavbarItem>
               <Link color="foreground" href="#" aria-current="page">
                 Register new Medics
               </Link>
@@ -75,20 +74,58 @@ function Navbar( ) {
           </NavbarContent>
         )}
 
-        <NavbarContent justify="end">
-          {isLoggedIn ? (
-            <>
-              <Avatar src="https://i.pravatar.cc/300" size="md" />
-              <NavbarItem>Josefino Calças</NavbarItem>
-            </>
-          ) : (
-            <>
-              <NavbarItem className="hidden lg:flex">
-                <Link href="/login">Login</Link>
-              </NavbarItem>
-            </>
-          )}
-        </NavbarContent>
+        {user ? (
+          <NavbarContent as="div" justify="end">
+            <Dropdown placement="bottom-end">
+              <DropdownTrigger>
+                <span className="flex flex-row items-center cursor-pointer">
+                  <Avatar
+                    isBordered
+                    as="button"
+                    className="mr-4 text-xl transition-transform"
+                    color="secondary"
+                    name={user}
+                    size="sm"
+                    src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                  />
+                  <h1 className="font-semibold text-primary">{user}</h1>
+                </span>
+              </DropdownTrigger>
+              <DropdownMenu aria-label="Profile Actions" variant="flat">
+                <DropdownItem key="profile" className="gap-2 h-14">
+                  <p className="font-semibold">Signed in as</p>
+                  <p className="font-semibold">zoey@example.com</p>
+                </DropdownItem>
+                <DropdownItem key="settings">My Settings</DropdownItem>
+                <DropdownItem key="team_settings">Team Settings</DropdownItem>
+                <DropdownItem key="analytics">Analytics</DropdownItem>
+                <DropdownItem key="system">System</DropdownItem>
+                <DropdownItem key="configurations">Configurations</DropdownItem>
+                <DropdownItem key="help_and_feedback">
+                  Help & Feedback
+                </DropdownItem>
+                <DropdownItem
+                  key="logout"
+                  color="danger"
+                  as="button"
+                  onClick={logout}
+                >
+                <span className="flex flex-row items-center gap-2 text-danger">
+                  
+                  <CgLogOut />
+                  Log Out
+                </span>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarContent>
+        ) : (
+          <NavbarItem className="hidden lg:flex">
+            <Link href="/login">Login</Link>
+          </NavbarItem>
+        )}
+
+        
       </Nav>
     </nav>
   );
