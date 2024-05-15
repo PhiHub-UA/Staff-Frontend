e488498e207c4280c29a6152d07ce319b3c0b3ca
FiNeX96 <rodrigoaguiar96@ua.pt>
2024-05-15 16:59:37 +0100

    PHIH-43 fix login

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -5,47 +5,51 @@ import {
   CardFooter,
   Button,
   Input,
-} from "@nextui-org/react";
+} from '@nextui-org/react';
 
-import Navbar from "../layout/Navbar";
-import Footer from "../layout/Footer";
+import Navbar from '../layout/Navbar';
+import Footer from '../layout/Footer';
 
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useMutation } from "@tanstack/react-query";
-import axios from "../../api/axios";
+import * as yup from 'yup';
+import {useForm} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup';
+import {useMutation} from '@tanstack/react-query';
+import axios from '../../api/axios';
+import {useNavigate} from 'react-router-dom';
 
-function LoginPage() {
-  const loginSchema = yup.object().shape({
-    username: yup.string().required(),
-    password: yup.string().required(),
+function LoginPage () {
+  const navigate = useNavigate ();
+
+  const loginSchema = yup.object ().shape ({
+    username: yup.string ().required (),
+    password: yup.string ().required (),
   });
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(loginSchema),
+  const {register, handleSubmit, formState: {errors}} = useForm ({
+    resolver: yupResolver (loginSchema),
   });
 
-  function onSubmit(data) {
-    login.mutate(data);
+  function onSubmit (data) {
+    login.mutate (data);
   }
 
-  const login = useMutation({
-    mutationKey: ["login"],
-    mutationFn: async (data) => {
-      await axios.post(
-        "/auth/login",
-        { ...data, role: "staff" },
+  const login = useMutation ({
+    mutationKey: ['login'],
+    mutationFn: async data => {
+      await axios.post (
+        '/auth/login',
+        {...data, role: 'staff'},
         {
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
         }
       );
+    },
+    onSuccess: () => {
+      setTimeout (() => {
+        navigate ('/dashboard');
+      }, 2000);
     },
   });
 
@@ -58,21 +62,27 @@ function LoginPage() {
             <h1 className="text-2xl font-bold text-primary">Login</h1>
           </CardHeader>
 
-          <form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <form noValidate onSubmit={handleSubmit (onSubmit)}>
             <CardBody className="gap-2">
-            <span className="flex flex-col">
-              <Input placeholder="Username" {...register("username")} />
-              {errors.username && <p className="p-1 px-2 text-error text-md">{errors.username.message} </p>}
-            </span>
-            <span className="flex flex-col">
+              <span className="flex flex-col">
+                <Input placeholder="Username" {...register ('username')} />
+                {errors.username &&
+                  <p className="p-1 px-2 text-error text-md">
+                    {errors.username.message}{' '}
+                  </p>}
+              </span>
+              <span className="flex flex-col">
 
-              <Input
-                type="password"
-                placeholder="Password"
-                {...register("password")}
-              />
-              {errors.password && <p className="p-1 px-2 text-error text-md">{errors.password.message} </p>}
-            </span>
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  {...register ('password')}
+                />
+                {errors.password &&
+                  <p className="p-1 px-2 text-error text-md">
+                    {errors.password.message}{' '}
+                  </p>}
+              </span>
             </CardBody>
 
             <CardFooter className="flex flex-col justify-evenly">
@@ -80,8 +90,14 @@ function LoginPage() {
                 Login
               </Button>
 
-              {login.isError && <p className="p-1 px-2 text-error text-md">{login.error.message}</p>}
-              {login.isSuccess && <p className="p-1 px-2 text-success text-md"> Registo efetuado com sucesso </p>}
+              {login.isError &&
+                <p className="p-1 px-2 text-error text-md">
+                  {login.error.message}
+                </p>}
+              {login.isSuccess &&
+                <p className="p-1 px-2 text-success text-md">
+                  {' '}Registo efetuado com sucesso{' '}
+                </p>}
             </CardFooter>
           </form>
         </Card>
