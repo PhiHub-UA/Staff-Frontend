1548172c4ac16fafb81322b8f83cde4a6ed2ac9a
d479 <daniel.madureira@ua.pt>
2024-05-15 12:41:37 +0100

    fixed error on login

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -5,41 +5,47 @@ import {
   CardFooter,
   Button,
   Input,
-} from '@nextui-org/react';
+} from "@nextui-org/react";
 
-import Navbar from '../layout/Navbar';
-import Footer from '../layout/Footer';
+import Navbar from "../layout/Navbar";
+import Footer from "../layout/Footer";
 
-import * as yup from 'yup';
-import {useForm} from 'react-hook-form';
-import {yupResolver} from '@hookform/resolvers/yup';
-import {useMutation} from '@tanstack/react-query';
-import axios from '../../api/axios';
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useMutation } from "@tanstack/react-query";
+import axios from "../../api/axios";
 
-function LoginPage () {
-
-
+function LoginPage() {
   const loginSchema = yup.object().shape({
-    username: yup.string ().required (),
-    password: yup.string ().required(),
+    username: yup.string().required(),
+    password: yup.string().required(),
   });
 
-  const {register, handleSubmit, formState: {errors}} = useForm ({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(loginSchema),
   });
 
-  function onSubmit (data) {
+  function onSubmit(data) {
     login.mutate(data);
   }
 
-  const login = useMutation ({
-    mutationKey: ['login'],
-    mutationFn: async data => {
-      await axios.post ('/auth/login', {... data, role: "staff"} , {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+  const login = useMutation({
+    mutationKey: ["login"],
+    mutationFn: async (data) => {
+      await axios.post(
+        "/auth/login",
+        { ...data, role: "staff" },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
     },
   });
 
@@ -52,25 +58,30 @@ function LoginPage () {
             <h1 className="text-2xl font-bold text-primary">Login</h1>
           </CardHeader>
 
-          <form noValidate onSubmit={handleSubmit (onSubmit)}>
+          <form noValidate onSubmit={handleSubmit(onSubmit)}>
             <CardBody className="gap-2">
-              <Input placeholder="Username" {...register ('username')} />
-              {errors.username && <p>{errors.username.message} </p>}
+            <span className="flex flex-col">
+              <Input placeholder="Username" {...register("username")} />
+              {errors.username && <p className="p-1 px-2 text-error text-md">{errors.username.message} </p>}
+            </span>
+            <span className="flex flex-col">
+
               <Input
                 type="password"
                 placeholder="Password"
-                {...register ('password')}
+                {...register("password")}
               />
-              {errors.password && <p>{errors.password.message} </p>}
+              {errors.password && <p className="p-1 px-2 text-error text-md">{errors.password.message} </p>}
+            </span>
             </CardBody>
 
-            <CardFooter className="flex justify-evenly">
+            <CardFooter className="flex flex-col justify-evenly">
               <Button color="primary" variant="flat" block type="submit">
                 Login
               </Button>
 
-              {login.isError && <p>{login.error.message}</p>}
-              {login.isSuccess && <p> Registo efetuado com sucesso </p>}
+              {login.isError && <p className="p-1 px-2 text-error text-md">{login.error.message}</p>}
+              {login.isSuccess && <p className="p-1 px-2 text-success text-md"> Registo efetuado com sucesso </p>}
             </CardFooter>
           </form>
         </Card>
